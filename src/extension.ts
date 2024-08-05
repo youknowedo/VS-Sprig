225d3058e40043d586b744fc7c0bba225fcb864b
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-05 11:52:33 +0200

    switch from in webview console to using vscode output

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import * as vscode from "vscode";
 
 let panel: vscode.WebviewPanel | null = null;
 let sprigFilename: string | null = null;
+let orange: vscode.OutputChannel | null = null;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -11,6 +12,12 @@ export function activate(context: vscode.ExtensionContext) {
   let openCommand = vscode.commands.registerTextEditorCommand(
     "sprig.open",
     (editor) => {
+      if (!orange) {
+        orange = vscode.window.createOutputChannel("Sprig");
+      }
+
+      orange.show();
+
       if (panel) {
         panel.reveal();
         return;
@@ -32,6 +39,17 @@ export function activate(context: vscode.ExtensionContext) {
       sprigFilename = editor.document.fileName;
 
       panel.webview.html = getWebviewContent(gameCode);
+      panel.webview.onDidReceiveMessage(
+        (message) => {
+          switch (message.command) {
+            case "log":
+              orange?.appendLine(message.text);
+              return;
+          }
+        },
+        undefined,
+        context.subscriptions
+      );
 
       panel.onDidDispose(
         () => {
@@ -90,36 +108,6 @@ function getWebviewContent(gameCode: string) {
             outline: none;
           }
 
-          h2 {
-            font-family: sans-serif;
-            color: #078969;
-            font-weight: bold;
-            text-align: center;
-            margin-top: 10px;
-            margin-bottom: 5px;
-          }
-
-          #output {
-            font-family: monospace;
-            background-color: #000;
-            border: 3px solid #078969;
-            height: 100%;
-            font-size: 16px;
-            padding: 4px;
-            overflow-y: scroll;
-            width: 100%;
-            margin: 0;
-          }
-
-          #output * {
-            overflow-anchor: none;
-          }
-
-          #anchor {
-            overflow-anchor: auto;
-            height: 1px;
-          }
-
           #actions {
             display: flex;
             justify-content: center;
@@ -142,24 +130,25 @@ function getWebviewContent(gameCode: string) {
         </style>
       </head>
       <body>
-        <h2>Game</h2>
         <canvas width="500" height="400" id="canvas" tabindex="0"></canvas>
         <div id="actions">
           <button id="restart">Restart</button>
           <button id="stop">Stop</button>
         </div>
-        <h2>Console</h2>
-        <p id="output">
-          <span id="anchor"></span>
-        </p>
 
         <script type="module">
           import { webEngine } from "https://esm.sh/sprig@1/web";
+
+          const vscode = acquireVsCodeApi();
 
           function _addConsoleOutput() {
             const output = document.getElementById("output");
             const text = Array.from(arguments).join(" ");
             output.innerText += text + "\\n";
+            vscode.postMessage({
+              command: "log",
+              text: text
+            });
           }
 
           console.log = _addConsoleOutput;
